077cf618c8d3132426475cd659aa255966f1b83f
Salma <sazab638@gmail.com>
2025-09-13 10:46:32 +0300

    routes sperated from main

--- lib/main.dart
+++ lib/main.dart
@@ -2,31 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
-import 'package:movie_app/presentation/screens/profile_screen.dart';
-import 'package:movie_app/presentation/screens/update_profile_screen.dart';
+import 'package:movie_app/routing/app_routes.dart';
+import 'package:movie_app/ui/core/themes/app_colors.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-
-import 'ui/core/di/injection_container.dart' as di;
-import 'ui/core/themes/app_colors.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'l10n/gen/app_localizations.dart';
-
-import 'presentation/screens/language_provider.dart';
-import 'presentation/screens/splash_screen.dart';
-import 'presentation/screens/login_screen.dart';
-import 'presentation/screens/register_screen.dart';
-//import 'presentation/screens/onboarding_screen.dart';
-import 'presentation/screens/forget_password_screen.dart';
+import 'ui/core/di/injection_container.dart' as di;
 import 'presentation/cubits/auth/auth_cubit.dart';
+import 'presentation/screens/language_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Firebase init
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   await di.init();
+
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => LanguageProvider()),
-        BlocProvider(create: (_) => di.sl<AuthCubit>()),
+        BlocProvider<AuthCubit>(
+          create: (_) => di.sl<AuthCubit>()..getCurrentUser(),
+        ),
       ],
       child: const MyApp(),
     ),
@@ -46,22 +48,7 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-
-
-    _router = GoRouter(
-      initialLocation: '/splash',
-      routes: [
-        GoRoute(path: '/splash', builder: (_, __) => const SplashScreen()),
-        //GoRoute(path: '/onboarding', builder: (_, __) => const OnboardingScreen()),
-        GoRoute(path: '/login', builder: (_, __) => const LoginScreen()),
-        GoRoute(path: '/register', builder: (_, __) => const RegisterScreen()),
-        GoRoute(path: '/forgot-password', builder: (_, __) => const ForgotPasswordScreen()),
-        GoRoute(path: '/home', builder: (_, __) => const Placeholder()), // replace with HomeScreen
-        GoRoute(path: '/profile', builder: (_, __) => const ProfileScreen()),
-        GoRoute(path: '/profile/update', builder: (_, __) => const UpdateProfileScreen()),
-
-      ],
-    );
+    _router = AppRouters.create(context); // ✅ pass AuthCubit context
   }
 
   @override
