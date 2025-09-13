b3dc266b43c29d54b7b11edad40c8d559b3bf745
Salma <sazab638@gmail.com>
2025-09-13 10:42:49 +0300

    DI edited

--- lib/ui/core/di/injection_container.dart
+++ lib/ui/core/di/injection_container.dart
@@ -1,6 +1,5 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
-import 'package:injectable/injectable.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -10,68 +9,73 @@ import '../../../data/repositories/auth_repository_impl.dart';
 import '../../../data/datasources/auth_remote_datasource.dart';
 import '../../../data/datasources/local_datasource.dart';
 import '../../../presentation/cubits/auth/auth_cubit.dart';
-import '../../../ui/core/network/api_service.dart';
+import '../network/api_service.dart';
 import '../network/dio_client.dart';
-
 final sl = GetIt.instance;
 
 Future<void> init() async {
+  // ---------------------------
   // External dependencies
+  // ---------------------------
   final sharedPreferences = await SharedPreferences.getInstance();
   sl.registerLazySingleton<SharedPreferences>(() => sharedPreferences);
   sl.registerLazySingleton<InternetConnectionChecker>(
-        () => InternetConnectionChecker(),
-  );
+          () => InternetConnectionChecker());
 
-  // Authenticated Dio with token
-  sl.registerLazySingleton<Dio>(
-        () => DioClient.createAuthDio(sharedPreferences),
-  );
+  // ---------------------------
+  // Core (Dio + Retrofit ApiService)
+  // ---------------------------
+  sl.registerLazySingleton<Dio>(() => DioClient.createAuthDio(sharedPreferences));
+  sl.registerLazySingleton<ApiService>(() => ApiService(
+    sl<Dio>(),
+    baseUrl: "https://route-movie-apis.vercel.app/",
+  ));
 
-  // ApiService (Retrofit)
-  sl.registerLazySingleton<ApiService>(
-        () => ApiService(sl<Dio>()),
-  );
-
+  // ---------------------------
   // Data sources
+  // ---------------------------
   sl.registerLazySingleton<AuthRemoteDataSource>(
-        () => AuthRemoteDataSourceImpl(sl<ApiService>()),
-  );
-
+          () => AuthRemoteDataSourceImpl(sl<ApiService>()));
   sl.registerLazySingleton<LocalDataSource>(
-        () => LocalDataSourceImpl(sl<SharedPreferences>()),
-  );
+          () => LocalDataSourceImpl(sl<SharedPreferences>()));
 
+  // ---------------------------
   // Repository
-  sl.registerLazySingleton<AuthRepository>(
-        () => AuthRepositoryImpl(
-      sl<AuthRemoteDataSource>(),
-      sl<LocalDataSource>(),
-      sharedPreferences: sl<SharedPreferences>(),
-      connectionChecker: sl<InternetConnectionChecker>(),
-    ),
-  );
+  // ---------------------------
+  sl.registerLazySingleton<AuthRepository>(() => AuthRepositoryImpl(
+    sl<AuthRemoteDataSource>(),
+    sl<LocalDataSource>(),
+    sharedPreferences: sl<SharedPreferences>(),
+    connectionChecker: sl<InternetConnectionChecker>(),
+  ));
 
+  // ---------------------------
   // Use cases
+  // ---------------------------
   sl.registerLazySingleton(() => LoginUseCase(sl<AuthRepository>()));
   sl.registerLazySingleton(() => RegisterUseCase(sl<AuthRepository>()));
-  sl.registerLazySingleton(() => ForgotPasswordUseCase(sl<AuthRepository>()));
+  sl.registerLazySingleton(() => ResetPasswordUseCase(sl<AuthRepository>()));
   sl.registerLazySingleton(() => UpdateProfileUseCase(sl<AuthRepository>()));
   sl.registerLazySingleton(() => GetCurrentUserUseCase(sl<AuthRepository>()));
   sl.registerLazySingleton(() => LogoutUseCase(sl<AuthRepository>()));
   sl.registerLazySingleton(() => IsLoggedInUseCase(sl<AuthRepository>()));
   sl.registerLazySingleton(() => DeleteAccountUseCase(sl<AuthRepository>()));
+  sl.registerLazySingleton(() => GoogleLoginUseCase(
+    sl<AuthRepository>(),
+    sl<LocalDataSource>(),
+  ));
 
-  // Cubit
-  sl.registerFactory<AuthCubit>(
-        () => AuthCubit(
-      sl<LoginUseCase>(),
-      sl<RegisterUseCase>(),
-      sl<ForgotPasswordUseCase>(),
-      sl<UpdateProfileUseCase>(),
-      sl<GetCurrentUserUseCase>(),
-      sl<LogoutUseCase>(),
-      sl<DeleteAccountUseCase>(),
-    ),
-  );
+  // ---------------------------
+  // Cubits
+  // ---------------------------
+  sl.registerFactory<AuthCubit>(() => AuthCubit(
+    sl<LoginUseCase>(),
+    sl<RegisterUseCase>(),
+    sl<ResetPasswordUseCase>(),
+    sl<UpdateProfileUseCase>(),
+    sl<GetCurrentUserUseCase>(),
+    sl<LogoutUseCase>(),
+    sl<DeleteAccountUseCase>(),
+    sl<GoogleLoginUseCase>(),
+  ));
 }
